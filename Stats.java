dd4dd7a13f90072bb47ffcf20dd5adc1c058c6b4
apcs <apcs@mvhs-u52827.dcsdk12.org>
2013-09-24 08:41:24 -0600

    What did I do I give up

--- Stats.java
+++ Stats.java
@@ -2,14 +2,18 @@ public class Stats {
 	
 	public static void main(String[] args) {
 		
-		int[] a = {2,6,5,3,9,24,103,62};
+		int[] a = {1,3,5,9,24,57,105};
 
 		int maxi = max(a);
 		int mini = min(a);
+		double mean = average(a);
+		double med = median(a);
 
 
 		System.out.println("Maximum number in array: " + maxi);
 		System.out.println("Minimum number in array: " + mini);
+		System.out.println("Average of array: " + mean);
+		System.out.println("Median of array: " + med);
 	}
 
 
@@ -39,5 +43,33 @@ public class Stats {
 		}
 
 		return result;
+	}
+
+	public static double average(int[] a) {
+
+		double result = 0.0;
+
+		for (int i = 0; i < a.length; i++) {
+			result += a[i];
+		}
+		result /=a.length;
+
+		return result;
+	}
+
+	public static double median(int[] a) {
+
+		double median = 0.0;
+
+		for (int i = 0, j = a.length; i < a.length; i++, j--) {
+			if(a[i] == a[j]) {
+				median = a[i];
+				break;
+			}
+
+		}
+
+		return median;
+
 	}
 }
